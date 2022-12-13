8cc9a7e0393a338509cf661c9fb64cd2adb73132
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2022-12-13 15:03:58 +0300

    lab2 Добавил подсчёт количества ссылок и дату тестирования

--- lab2/app/linkcrawler/src/main/java/linkhamster/cmd/Main.java
+++ lab2/app/linkcrawler/src/main/java/linkhamster/cmd/Main.java
@@ -9,8 +9,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 public class Main
@@ -46,13 +49,33 @@ public class Main
             HashMap<URL, URL> hashMap = new HashMap<>();
             crawler.getSiteMap().forEach((from, to) -> to.forEach(toUrl -> hashMap.put(toUrl, from)));
 
+            AtomicInteger goodLinks = new AtomicInteger();
+            AtomicInteger badLinks = new AtomicInteger();
+
             crawler.getVisited().forEach((url, statusCode) -> {
                 var fromUrl = hashMap.get(url);
                 if (statusCode < 400)
+                {
+                    goodLinks.getAndIncrement();
                     goodLinksWriter.format("%d | %s | from: %s\n", statusCode, url, fromUrl);
+                }
                 else
+                {
+                    badLinks.getAndIncrement();
                     badLinksWriter.format("%d | %s | from: %s\n", statusCode, url, fromUrl);
+                }
             });
+
+            goodLinksWriter.format(
+                    "%d %s\n",
+                    goodLinks.get(),
+                    DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss").format(LocalDateTime.now())
+            );
+            badLinksWriter.format(
+                    "%d %s\n",
+                    badLinks.get(),
+                    DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss").format(LocalDateTime.now())
+            );
         }
         catch (FileNotFoundException e)
         {
